b00e9b273651f7b55f6d16d05470e44b1d762a16
StormKing2 <94218345+Master-Bizz@users.noreply.github.com>
2021-12-13 02:58:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+'''
+Conditions
+'''
+#x = 6
+#y = 8
+#if x < y:
+ # print("YES SIR")
+
+# if "hello" in "yehello my people!":
+#print("Indeed there is")
+
+# x = 8
+# y = 4
+# if x > y:
+ #  print("It's definitely greater")
+# elif x < y:
+#  print("It's lesser")  
+# else:
+#  print("Neither")    
+'''
+Nested If Statements
+'''
+# x = 8
+# num = False
+# if num >= 0:
+  # if num == 0:
+   # print("Zero Baby")
+ # else: 
+   # print("Its more special than Zero!")
+# else:
+ # print("It's a Negative number man!")    
+'''
+One-Line If Statements
+'''
+ # if x > 0: print("Greater than Zero!")
+'''
+Ternary operator
+'''
+# print('Yes') if 8 > 6 else print('Nope')
+# Samwe thing but the bottom example is more readable for users.
+# if 8 > 6:
+  # print('Its Greater!')
+# else:
+    # print('Nah')  
+
+# Type 'pass' to pause during statements if needed.
